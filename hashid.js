9787e655cf3c9db85fc24cbdc5fb5ebbd47e410e
Josh Taylor <joshrtay@gmail.com>
2014-09-09 11:10:52 -0700

    fix redis connection

--- hashid.js
+++ hashid.js
@@ -1,7 +1,8 @@
 var SIXDIGITOFFSET = 3748096;
+var REDIS_URL = process.env.REDISTOGO_URL || 'redis://localhost:6379';
 
 var hashids = new (require('hashids'))('themang');
-var redis = require('redis').createClient();
+var redis = require('redis-url').connect(REDIS_URL);
 
 module.exports = function(cb) {
   redis.incrby('temp-email:hashid', 1, function(err, id) {
